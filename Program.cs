7f675bbfdff2303043a7ff951ed91d02c90dd67b
MykhailoDavydenko <145581619+MykhailoDavydenko@users.noreply.github.com>
2024-02-15 23:13:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab_1_2;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Fraction fraction = new();
+        fraction.Read();
+        fraction.Display();
+
+        Console.WriteLine("\n Simplified fraction: ");
+        fraction.Simplify();
+        fraction.Display();
+
+        Console.WriteLine($"\n" + fraction.Calculate());
+
+        // MakeFraction
+        {
+            double Numerator;
+            double Denominator;
+
+            Console.Write("\nEnter Numerator: ");
+            Numerator = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Denominator: ");
+            Denominator = Convert.ToDouble(Console.ReadLine());
+            fraction = MakeFraction(Numerator, Denominator);
+
+            fraction.Display();
+        }
+    }
+    static Fraction MakeFraction(double first, double second)
+    {
+        Fraction fraction = new();
+        if (!fraction.Init(first, second))
+        {
+            Console.WriteLine("Wrong arguments to Init!");
+            return null;
+        }
+        else
+        {
+            return fraction;
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProject2_
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
